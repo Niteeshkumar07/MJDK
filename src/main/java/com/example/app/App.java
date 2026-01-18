c7d641b1ff30ecf939503707cea510d005b7eafd
Niteeshkumar07 <niteeshkumar4020@gmail.com>
2026-01-18 17:21:15 +0530

    Ok

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,9 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Niteesh");
+        System.out.println("Rahul");
+        System.out.println("Ankit");
+    }
+}
