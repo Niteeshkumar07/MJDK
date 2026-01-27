69cb4e42f992061c1d52caf41b54dea00b1f1eaf
Niteesh Kumar <121102529+Niteeshkumar07@users.noreply.github.com>
2026-01-27 15:10:10 +0530

    Replace 'Ankit' with 'hurray' in output

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Niteesh");
         System.out.println("Rahul");
-        System.out.println("Ankit");
+        System.out.println("hurray");
         System.out.flush();
     }
 }
