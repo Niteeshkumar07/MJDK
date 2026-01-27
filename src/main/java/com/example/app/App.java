453b697ebaa6ff72f12be4ba53d3bea7753d9806
Niteesh Kumar <121102529+Niteeshkumar07@users.noreply.github.com>
2026-01-27 15:43:31 +0530

    Update output message from 'hurray' to 'hurray la'

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Niteesh");
         System.out.println("Rahul");
-        System.out.println("hurray");
+        System.out.println("hurray la");
         System.out.flush();
     }
 }
